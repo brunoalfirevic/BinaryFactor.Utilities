46583bfc9785b1a380ecda994c6baff54187af70
Bruno Alfirević <bruno.alfirevic@gmail.com>
2020-04-04 21:20:16 +0200

    Add CustomAttributeProviderExtensions

---
+++ src/BinaryFactor.Utilities/CustomAttributeProviderExtensions.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Bruno Alfirević. All rights reserved.
+// Licensed under the MIT license. See license.txt in the project root for license information.
+
+namespace BinaryFactor.Utilities
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class CustomAttributeProviderExtensions
+    {
+        public static bool HasAttribute<TAttribute>(this ICustomAttributeProvider attributeProvider, bool inherit = false)
+            where TAttribute : class
+        {
+            return attributeProvider.GetAttribute<TAttribute>(inherit) != null;
+        }
+
+        public static TAttribute GetAttribute<TAttribute>(this ICustomAttributeProvider attributeProvider, bool inherit = false)
+            where TAttribute : class
+        {
+            return attributeProvider.GetAttributes<TAttribute>(inherit).FirstOrDefault();
+        }
+
+        public static IList<TAttribute> GetAttributes<TAttribute>(this ICustomAttributeProvider attributeProvider, bool inherit = false)
+            where TAttribute : class
+        {
+            var attributes = attributeProvider.GetCustomAttributes(inherit);
+
+            return attributes.OfType<TAttribute>().ToList();
+        }
+    }
+}
