42b2be48f25e3adef03054cfa2e969cdf40a9bea
Bruno Alfirević <bruno.alfirevic@gmail.com>
2020-04-30 02:10:40 +0200

    Add HashCodeHelper and StructurallyEquatable

---
+++ src/BinaryFactor.Utilities/HashCodeHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryFactor.Utilities
+{
+    public static class HashCodeHelper
+    {
+        public static int Combine<T>(IEnumerable<T> values)
+        {
+            unchecked
+            {
+                var hash = Combine(17, RandomSeed);
+
+                foreach (var value in values)
+                    hash = Combine(hash, value?.GetHashCode() ?? 0);
+
+                return hash;
+            }
+        }
+
+        public static int CombineUnordered<T>(IEnumerable<T> values)
+        {
+            var hash = RandomSeed;
+
+            foreach (var item in values)
+                hash = CombineUnordered(hash, item?.GetHashCode() ?? 0);
+
+            return hash;
+        }
+
+        private static int Combine(int h1, int h2) => h1 * 23 + h2;
+
+        private static int CombineUnordered(int h1, int h2) => h1 ^ h2;
+
+        private static readonly int RandomSeed = Guid.NewGuid().GetHashCode();
+    }
+}
---
+++ src/BinaryFactor.Utilities/StructurallyEquatable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinaryFactor.Utilities
+{
+    public abstract class StructurallyEquatable<T> : StructurallyEquatable, IEquatable<T>
+        where T: StructurallyEquatable
+    {
+        public bool Equals(T other) => Equals((object)other);
+    }
+
+    public abstract class StructurallyEquatable
+    {
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj.GetType() != GetType())
+                return false;
+
+            var objEqualityItems = ((StructurallyEquatable)obj).EqualsBy();
+            var thisEqualityItems = EqualsBy();
+
+            return IsSet(objEqualityItems) && IsSet(thisEqualityItems)
+                ? objEqualityItems.ToHashSet().SetEquals(thisEqualityItems)
+                : Enumerable.SequenceEqual(objEqualityItems, thisEqualityItems);
+        }
+
+        public override int GetHashCode()
+        {
+            var equalityItems = EqualsBy();
+
+            return IsSet(equalityItems)
+                ? HashCodeHelper.CombineUnordered(equalityItems)
+                : HashCodeHelper.Combine(equalityItems);
+        }
+
+        protected abstract IEnumerable<object> EqualsBy();
+
+        private bool IsSet<T>(IEnumerable<T> enumerable) => enumerable.GetType().IsAssignableToGenericType(typeof(ISet<>));
+    }
+}
